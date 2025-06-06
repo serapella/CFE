81e8203b5a429a92cf2e5b6f2651bd4dfed67642
Serap Cakmak <serap.cakmak@hotmail.com>
2025-06-06 18:04:02 +0200

    create page categories

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,1 +1,12 @@
- 
+import { NextResponse } from "next/server";
+import { categoryQueries } from "@/queries/categoryQueries";
+
+export async function GET() {
+  try {
+    const categories = await categoryQueries.getAll();
+    return NextResponse.json(categories);
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json({ error: e }, { status: 500 });
+  }
+}
